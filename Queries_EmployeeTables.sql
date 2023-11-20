dd7b978af028e34c1ffdddb8e39c96d6d9249735
Ao-27 <jowrk.oldacreajani@gmail.com>
2023-11-20 10:11:06 -0500

    Fixed Quick ERD Url Issues - Added Images+Txt file to Repo

--- Queries_EmployeeTables.sql
+++ Queries_EmployeeTables.sql
@@ -1,32 +1,36 @@
 -- List the employee number, last name, first name, sex, and salary of each employee.
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 
-FROM employees as e
+FROM
+employees as e
 
 INNER JOIN salaries as s ON s.emp_no = e.emp_no;
 	
 -- List the first name, last name, and hire date for the employees who were hired in 1986
-SELECT first_name, last_name, hire_date
+SELECT e.first_name, e.last_name, e.hire_date
 
-FROM employees
+FROM
+employees as e
 
 WHERE 
-date_part('year', hire_date) = 1986;
+date_part('year', e.hire_date) = 1986;
 
 -- List the manager of each department along with their department number, department name, employee number, 
 -- last name, and first name
-SELECT m.dept_no, d.dept_name, m.emp_no, e.last_name, e.first_name
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
 
-FROM dept_manager as m
+FROM
+dept_manager as dm
 
 INNER JOIN departments as d ON m.dept_no = d.dept_no 
 INNER JOIN employees as e ON m.emp_no = e.emp_no;
 
 --List the department number for each employee along with that employee’s employee
 -- number, last name, first name, and department name
-SELECT n.dept_no, e.emp_no, e.last_name, e.first_name, d.dept_name
+SELECT de.dept_no, e.emp_no, e.last_name, e.first_name, d.dept_name
 
-FROM dept_emp as n
+FROM
+dept_emp as de
 
 INNER JOIN employees as e ON n.emp_no = e.emp_no
 INNER JOIN departments as d ON n.dept_no = d.dept_no;
@@ -35,7 +39,8 @@ INNER JOIN departments as d ON n.dept_no = d.dept_no;
 -- whose last name begins with the letter B
 SELECT e.first_name, e.last_name, e.sex
 
-FROM employees as e
+FROM
+employees as e
 
 WHERE
 e.first_name = 'Hercules' 
@@ -46,12 +51,12 @@ e.last_name LIKE 'B%';
 -- name, and first name
 SELECT
 n.emp_no, e.last_name, e.first_name
-FROM dept_emp as n
+
+FROM
+dept_emp as n
 
 INNER JOIN employees as e ON n.emp_no = e.emp_no
-
-INNER JOIN departments as d
-ON n.dept_no = d.dept_no
+INNER JOIN departments as d ON n.dept_no = d.dept_no
 
 WHERE 
 d.dept_name = 'Sales';
@@ -70,10 +75,10 @@ d.dept_name IN ('Sales', 'Development');
 
 -- List the frequency counts, in descending order, of all the employee last names (that is
 -- how many employees share each last name)
-SELECT last_name, COUNT(*) AS "Frquency_shared_lnames"
+SELECT e.last_name, COUNT(*) AS "frequency_shared_lnames"
 
-FROM Employees
+FROM employees as e
 
 GROUP BY last_name
 
-ORDER BY "Frquency_shared_lnames" DESC;
+ORDER BY "frequency_shared_lnames" DESC;
